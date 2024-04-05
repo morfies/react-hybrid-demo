9dd1422534b6c6ca327e9e6250d2402609aa4cfc
Yuefei Ma <yuefei.ma@shopee.com>
2024-04-05 10:43:17 +0800

    refine server build

--- webpack.server.js
+++ webpack.server.js
@@ -8,7 +8,7 @@ module.exports = {
   // in order to ignore built-in modules like path, fs, etc.
   externalsPresets: { node: true },
   // All node modules will no longer be bundled but will be left as require('module')
-  // externals: [nodeExternals()],
+  externals: [nodeExternals()],
   output: {
     path: path.resolve('server-build'),
     filename: 'index.js',
